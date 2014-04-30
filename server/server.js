cdcf14e0c77af78fa7645ead7bd26fce9a4c88e2
Jon Buckley <jon@jbuckley.ca>
2014-04-30 14:25:11 -0700

    Add CORS headers so we can play with this data

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ module.exports = function(redis_client) {
     name: "park-warden"
   });
 
+  server.use(restify.CORS({
+    origins: ['*']
+  }));
+
   server.get("/api/contributions/:bucket/:date", routes.contributors(redis_client));
   server.get("/api/referrers", routes.referrers(redis_client));
 
